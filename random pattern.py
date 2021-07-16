acb44942948b750286b80c9faea37a3d2e9b2fd1
kety-folf <keatonstorer@gmail.com>
2021-07-16 18:52:16 +1200

    Add files via upload
    upload random turtle program

---
+++ random pattern.py
@@ -0,0 +1,25 @@
+from turtle import *
+color('#80b3ff')
+
+from random import randint
+def infill ():
+    i = 0
+    while(i < 100):
+        i = i + 1
+        forward(76)
+        speed (randint(8,10)
+        circle (randint(7,40))
+        forward(randint(7,111))
+        left(67)
+        forward(randint(7,47))
+        right(216)
+        pensize(randint(1,10))
+        forward(randint(7,111))
+        left(176)
+        forward(48)
+        right(137)
+        circle (randint(7,111))
+        left (21)
+        circle (randint(7,111))
+    
+infill()
